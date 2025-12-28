7d6c7860e249849beccdadf535b8564093d4beb1
Rafa Ramos <soyrafaramos@gmail.com>
2025-12-28 16:15:16 +0100

    Usa imágenes de personajes en el tablero

--- js/poderes.js
+++ js/poderes.js
@@ -1,1 +1,26 @@
-
+const poderes = {
+  avispa: {
+    activos: [],
+    pasivos: [],
+  },
+  hulk: {
+    activos: [],
+    pasivos: [],
+  },
+  capitan: {
+    activos: [],
+    pasivos: [],
+  },
+  spiderman: {
+    activos: [],
+    pasivos: [],
+  },
+  lobezno: {
+    activos: [],
+    pasivos: [],
+  },
+  ciclope: {
+    activos: [],
+    pasivos: [],
+  },
+};
